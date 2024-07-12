a0991967bb898887001eca60dd9e93a0942f8f44
PAN-WICHAYA <wichaya@automationfirst.co.th>
2024-07-12 23:53:50 +0700

    Pagination control & derived state

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,18 +10,33 @@ import JobItemContent from './JobItemContent';
 import Sidebar, { SidebarTop } from './Sidebar';
 import ResultsCount from './ResultsCount';
 import JobList from './JobList';
-import PaginationControls from './PaginationControls';
+
 import SortingControls from './SortingControls';
 import { useDebounce, useJobItems } from '../lib/hooks';
 import { Toaster } from 'react-hot-toast';
+import PaginationControls from './PaginationControls';
 
 function App() {
+  // State
   const [searchText, setSearchText] = useState('');
   const deBounceSearchText = useDebounce(searchText, 250);
   const { jobItems, isLoading } = useJobItems(deBounceSearchText);
+  const [currentPage, setCurrentPage] = useState(1);
 
+  // derived state
   const totalNumberOfResults = jobItems?.length || 0;
-  const jobItemsSliced = jobItems?.slice(0, 7) || [];
+  const totalNumberOfPages = totalNumberOfResults / 7;
+  const jobItemsSliced =
+    jobItems?.slice(7 * currentPage - 7, currentPage * 7) || [];
+
+  // event handlers
+  const handleChangePage = (direction: 'next' | 'previous') => {
+    if (direction === 'next') {
+      setCurrentPage((prev) => prev + 1);
+    } else if (direction === 'previous') {
+      setCurrentPage((prev) => prev - 1);
+    }
+  };
   return (
     <>
       <Background />
@@ -43,7 +58,11 @@ function App() {
             <SortingControls />
           </SidebarTop>
           <JobList jobItems={jobItemsSliced} isLoading={isLoading} />
-          <PaginationControls />
+          <PaginationControls
+            onClick={handleChangePage}
+            currentPage={currentPage}
+            totalNumberOfPages={totalNumberOfPages}
+          />
         </Sidebar>
         <JobItemContent />
       </Container>
--- src/components/PaginationControls.tsx
+++ src/components/PaginationControls.tsx
@@ -1,3 +1,67 @@
-export default function Pagination() {
-  return <section className="pagination"></section>;
+import { ArrowLeftIcon, ArrowRightIcon } from '@radix-ui/react-icons';
+
+type PaginationControlProps = {
+  currentPage: number;
+  onClick: (direction: 'next' | 'previous') => void;
+  totalNumberOfPages: number;
+};
+
+export default function PaginationControls({
+  onClick,
+  currentPage,
+  totalNumberOfPages,
+}: PaginationControlProps) {
+  return (
+    <section className="pagination">
+      {currentPage > 1 && (
+        <PageinationButton
+          direction={'previous'}
+          currentPage={currentPage}
+          onClick={() => onClick('previous')}
+        />
+      )}
+      {currentPage < totalNumberOfPages && (
+        <PageinationButton
+          direction={'next'}
+          currentPage={currentPage}
+          onClick={() => onClick('next')}
+        />
+      )}
+    </section>
+  );
+}
+
+type PaginationButtonProps = {
+  direction: 'next' | 'previous';
+  currentPage: number;
+  onClick: () => void;
+};
+
+function PageinationButton({
+  direction,
+  currentPage,
+  onClick,
+}: PaginationButtonProps) {
+  return (
+    <button
+      onClick={(e) => {
+        onClick();
+        e.currentTarget.blur();
+      }}
+      className={`pagination__button pagination__button--${direction}`}
+    >
+      {direction === 'previous' && (
+        <>
+          <ArrowLeftIcon />
+          Page {currentPage - 1}
+        </>
+      )}
+      {direction === 'next' && (
+        <>
+          <ArrowRightIcon />
+          Page {currentPage + 1}
+        </>
+      )}
+    </button>
+  );
 }
